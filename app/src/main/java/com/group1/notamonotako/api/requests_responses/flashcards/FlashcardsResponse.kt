4ca29020ac6c5c2af30973380b1d35d8b718e5d3
Auxilium <PaulVillacote.PGMR@gmail.com>
2024-09-05 19:06:11 +0800

    flashcards request/respond

--- app/src/main/java/com/group1/notamonotako/api/requests_responses/flashcards/FlashcardsResponse.kt
+++ app/src/main/java/com/group1/notamonotako/api/requests_responses/flashcards/FlashcardsResponse.kt
@@ -1,8 +1,12 @@
 package com.group1.notamonotako.api.requests_responses.flashcards
 
+import com.google.gson.annotations.SerializedName
+
 data class FlashcardsResponse(
-    val id: Int
-    /*
-    To be continued
-     */
+    val id: Int,
+    val title: String,
+    val cards: List<String>,
+    val public: Boolean,
+    @SerializedName("to_public")
+    val toPublic: Boolean
 )

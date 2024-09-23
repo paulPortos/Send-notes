0891ff6f16d23e9e611d1821a06d0535a0d1f3ef
NooisY <143975472+lN00ISYl@users.noreply.github.com>
2024-09-23 18:58:43 +0800

    added a branch to the changepass

--- app/src/main/java/com/group1/notamonotako/views/SignUpActivity.kt
+++ app/src/main/java/com/group1/notamonotako/views/SignUpActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -96,6 +97,7 @@ class SignUpActivity : AppCompatActivity() {
             }.onFailure { error ->
                 // Handle registration errors
                 Toast.makeText(this@SignUpActivity, "Error: ${error.message}", Toast.LENGTH_SHORT).show()
+                Log.d("TESTER", "Error: ${error.message}")
             }
         }
     }

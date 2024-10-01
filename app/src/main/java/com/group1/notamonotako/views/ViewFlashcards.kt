5d130b4cf66d17a7498cd23a4cc80b7f9bd5e988
gonbeqt <143175525+gonbeqt@users.noreply.github.com>
2024-10-01 08:42:52 +0800

    ADDED VIEWFLASHCARDS

---
+++ app/src/main/java/com/group1/notamonotako/views/ViewFlashcards.kt
@@ -0,0 +1,16 @@
+package com.group1.notamonotako.views
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.group1.notamonotako.R
+
+class ViewFlashcards : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_flashcards)
+
+    }
+}

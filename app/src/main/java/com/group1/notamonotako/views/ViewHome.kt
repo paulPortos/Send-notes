431951fc5754fda153f8601ea4af822ad00f42ce
Auxilium <PaulVillacote.PGMR@gmail.com>
2024-10-06 01:55:06 +0800

    Update ViewHome.kt

--- app/src/main/java/com/group1/notamonotako/views/ViewHome.kt
+++ app/src/main/java/com/group1/notamonotako/views/ViewHome.kt
@@ -2,10 +2,12 @@ package com.group1.notamonotako.views
 
 import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.ImageButton
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,6 +20,10 @@ class ViewHome : AppCompatActivity() {
     private lateinit var btnComment : ImageButton
     private lateinit var flComment : FrameLayout
     private lateinit var btnClose : ImageButton
+    private lateinit var tvTitle : TextView
+    private lateinit var tvContents : TextView
+    private lateinit var tvDate : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_home)
@@ -25,6 +31,25 @@ class ViewHome : AppCompatActivity() {
         btnLike = findViewById(R.id.btnLike)
         btnDisLike = findViewById(R.id.btnDisLike)
         btnComment = findViewById(R.id.btnComment)
+        tvTitle = findViewById(R.id.tvTitle)
+        tvContents = findViewById(R.id.tvContents)
+        tvDate = findViewById(R.id.tvDate)
+
+        val adminId = intent.getIntExtra("admin_id", -1)
+        val noteId = intent.getIntExtra("note_id", -1)
+        val title = intent.getStringExtra("title")
+        val creatorUsername = intent.getStringExtra("creator_username")
+        val creatorEmail = intent.getStringExtra("creator_email")
+        val contents = intent.getStringExtra("contents")
+        val public = intent.getBooleanExtra("public", false)
+        val updatedAt = intent.getStringExtra("updated_at")
+
+        //log all
+        Log.d("ViewHome", "adminId: $adminId, noteId: $noteId, title: $title, creatorUsername: $creatorUsername, creatorEmail: $creatorEmail, contents: $contents, public: $public, updatedAt: $updatedAt")
+        tvTitle.text = title ?: "No title"
+        tvContents.text = contents ?: "No contents"
+        tvDate.text = updatedAt ?: "No date"
+
         flComment = findViewById(R.id.flComment)
         btnClose = findViewById(R.id.btnClose)
 
@@ -32,9 +57,7 @@ class ViewHome : AppCompatActivity() {
         btnLike.setOnClickListener {
 
         }
-
-
-
+        
         btnComment.setOnClickListener {
             // Show the comment frame with a slide-up animation
             flComment.visibility = View.VISIBLE

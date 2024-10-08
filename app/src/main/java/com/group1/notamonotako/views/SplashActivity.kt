d2b0117a0626352711e3178faffc1b14ff8ec246
gonbeqt <143175525+gonbeqt@users.noreply.github.com>
2024-10-08 22:07:16 +0800

    Added new intro

--- app/src/main/java/com/group1/notamonotako/views/SplashActivity.kt
+++ app/src/main/java/com/group1/notamonotako/views/SplashActivity.kt
@@ -2,28 +2,42 @@ package com.group1.notamonotako.views
 
 import android.content.Intent
 import android.content.pm.ActivityInfo
+import android.net.Uri
+import android.net.Uri.parse
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import android.widget.MediaController
+import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
 import com.group1.notamonotako.R
+import java.net.URI
 
 class SplashActivity : AppCompatActivity() {
-    private lateinit var imageView: ImageView
+    private lateinit var videoView: VideoView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        imageView = findViewById(R.id.noteszoom)
-        val zoomInAnimation = AnimationUtils.loadAnimation(this, R.anim.zoom_in)
-        imageView.startAnimation(zoomInAnimation)
+
+       videoView = findViewById(R.id.videoView)
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+
+        val uri: Uri = Uri.parse("android.resource://$packageName/${R.raw.intro}")
+        videoView.setMediaController(mediaController)
+        videoView.setVideoURI(uri)
+        videoView.requestFocus()
+        videoView.start()
+
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
             finish()
-        }, 2000)
+        }, 4500)
 
     }
 }

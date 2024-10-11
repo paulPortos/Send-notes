03093fbc1ed8dee96de74525288a87a07f1bb755
gonbeqt <143175525+gonbeqt@users.noreply.github.com>
2024-10-11 20:44:38 +0800

    Added function OnLongPress on the notesadapter

--- app/src/main/java/com/group1/notamonotako/adapter/MyNotesAdapter.kt
+++ app/src/main/java/com/group1/notamonotako/adapter/MyNotesAdapter.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
@@ -15,9 +16,11 @@ import com.group1.notamonotako.views.ViewMynotes
 class MyNotesAdapter(val context: Context, val notelist: List<Note>) : RecyclerView.Adapter<MyNotesAdapter.NotesViewHolder>() {
 
     inner class NotesViewHolder(noteView : View) : RecyclerView.ViewHolder(noteView) {
+        val checkbox: CheckBox = noteView.findViewById(R.id.note_checkbox)
         val title: TextView = noteView.findViewById(R.id.title)
         val notes : ConstraintLayout = noteView.findViewById(R.id.layout_notes)
         val contents: TextView = noteView.findViewById(R.id.contents)
+        val public : TextView = noteView.findViewById(R.id.tvPublic)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotesViewHolder {
@@ -41,6 +44,18 @@ class MyNotesAdapter(val context: Context, val notelist: List<Note>) : RecyclerV
             intent.putExtra("to_public", item.toPublic)
             it.context.startActivity(intent)
         }
+
+        holder.notes.setOnLongClickListener {
+            if (holder.checkbox.visibility == View.VISIBLE) {
+                holder.checkbox.visibility = View.GONE
+                holder.checkbox.isChecked = false
+            } else {
+                holder.checkbox.visibility = View.VISIBLE
+                holder.checkbox.isChecked = true
+            }
+            true // Return true to indicate the long press was handled
+        }
+
     }
 
     override fun getItemCount(): Int {

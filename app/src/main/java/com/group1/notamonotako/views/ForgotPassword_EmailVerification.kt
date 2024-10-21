4f2d58d5f01e27a2bb9c9fbfee364a8ce50c9211
Auxilium <PaulVillacote.PGMR@gmail.com>
2024-10-21 21:55:08 +0800

    ADDED PROGRESSBAR

--- app/src/main/java/com/group1/notamonotako/views/ForgotPassword_EmailVerification.kt
+++ app/src/main/java/com/group1/notamonotako/views/ForgotPassword_EmailVerification.kt
@@ -5,9 +5,12 @@ import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
@@ -26,7 +29,9 @@ class ForgotPassword_EmailVerification : AppCompatActivity() {
     private lateinit var textInputLayout: TextInputLayout
     private lateinit var btnGetOTP : AppCompatButton
     private lateinit var btnConfirmVerification :AppCompatButton
-
+    private lateinit var progressBar: ProgressBar
+    private var progressStatus = 0
+    private val handler = Handler(Looper.getMainLooper())
 
     // Variable to store the token received from the server
     private var OTP: String? = null
@@ -46,11 +51,15 @@ class ForgotPassword_EmailVerification : AppCompatActivity() {
         btnGetOTP = findViewById(R.id.btnGetOTP)
         btnBack = findViewById( R.id.btnBack)
         btnConfirmVerification = findViewById(R.id.btnConfirmVerification)
-
+        progressBar = findViewById(R.id.progressBar)
+        progressBar.visibility = View.GONE
+        progressBar.max = 5  // Set max value to 5 for 5 seconds
         textInputLayout.visibility = View.INVISIBLE
 
 
         btnGetOTP.setOnClickListener {
+            progressBar.visibility = View.VISIBLE
+
             lifecycleScope.launch {
                 btnConfirmVerification.isClickable = true
                 val email = etEmail.text.toString().trim()
@@ -61,6 +70,8 @@ class ForgotPassword_EmailVerification : AppCompatActivity() {
                 if (email.isEmpty()){
                     Toast.makeText(this@ForgotPassword_EmailVerification, "Please enter your email", Toast.LENGTH_SHORT).show()
                     btnConfirmVerification.isClickable = true
+                    progressBar.visibility = View.GONE
+
 
                 }else{
                     try {
@@ -78,18 +89,24 @@ class ForgotPassword_EmailVerification : AppCompatActivity() {
                             btnGetOTP.isClickable = false
                             Toast.makeText(this@ForgotPassword_EmailVerification, "OTP sent", Toast.LENGTH_SHORT).show()
                             textInputLayout.visibility = View.VISIBLE
+                            progressBar.visibility = View.GONE
+
                             OTPcountdown()
                         }else if(response.code() == 404){
                             btnConfirmVerification.isClickable = true
+                            progressBar.visibility = View.GONE
 
                             Toast.makeText(this@ForgotPassword_EmailVerification, "email not found", Toast.LENGTH_SHORT).show()
                         }else{
                             btnConfirmVerification.isClickable = true
+                            progressBar.visibility = View.GONE
 
                             Toast.makeText(this@ForgotPassword_EmailVerification, "Something went wrong", Toast.LENGTH_SHORT).show()
                         }
                     }   catch (e: IOException){
                         btnConfirmVerification.isClickable = true
+                        progressBar.visibility = View.GONE
+
 
                         Toast.makeText(this@ForgotPassword_EmailVerification, "Network error: ${e.message}", Toast.LENGTH_SHORT).show()
                     }
@@ -128,14 +145,20 @@ class ForgotPassword_EmailVerification : AppCompatActivity() {
         object : CountDownTimer(60000,1000){
             override fun onTick(millisUntilFinished : Long) {
                 btnGetOTP.text = "${millisUntilFinished / 1000} seconds "
+                progressBar.visibility = View.GONE
+
+
             }
             override fun onFinish() {
                 btnGetOTP.text = "GET OTP "
                 btnGetOTP.isEnabled = true
                 btnGetOTP.isClickable = true
                 textInputLayout.visibility = View.INVISIBLE
+
             }
         }.start()
 
     }
+
+
 }
